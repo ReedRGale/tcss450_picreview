bc31a58ee79ae117616698073d75a63e1e321e97
Jared Lowery <jadersleet@gmail.com>
2017-04-27 20:19:31 -0700

    Added another XML and Fragment pair (Location Picker)

---
+++ app/src/main/java/group1/tcss450/uw/edu/picreview/LocationPickerFragment.java
@@ -0,0 +1,85 @@
+package group1.tcss450.uw.edu.picreview;
+
+import android.content.Context;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ * This is designed to allow the user to pick a location for the picture.
+ * No location by default.
+ */
+public class LocationPickerFragment extends Fragment {
+
+    private OnFragmentInteractionListener mListener;
+
+    public LocationPickerFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_location_picker, container, false);
+    }
+
+
+    public void onMyLocationPressed(Uri uri) {
+        // TODO: Not yet implemented.
+
+        if (mListener != null) {
+            mListener.onFragmentInteraction(uri);
+        }
+    }
+
+    public void onSearchLocationPressed(Uri uri) {
+        // TODO: Not yet implemented.
+
+        if (mListener != null) {
+            mListener.onFragmentInteraction(uri);
+        }
+    }
+
+    public void onNoLocationPressed(Uri uri) {
+        // TODO: Not yet implemented.
+
+        if (mListener != null) {
+            mListener.onFragmentInteraction(uri);
+        }
+    }
+
+    @Override
+    public void onAttach(Context context) {
+        super.onAttach(context);
+        if (context instanceof OnFragmentInteractionListener) {
+            mListener = (OnFragmentInteractionListener) context;
+        } else {
+            throw new RuntimeException(context.toString()
+                    + " must implement OnFragmentInteractionListener");
+        }
+    }
+
+    @Override
+    public void onDetach() {
+        super.onDetach();
+        mListener = null;
+    }
+
+    /**
+     * This interface must be implemented by activities that contain this
+     * fragment to allow an interaction in this fragment to be communicated
+     * to the activity and potentially other fragments contained in that
+     * activity.
+     */
+    public interface OnFragmentInteractionListener {
+        // TODO: Update argument type and name
+        void onFragmentInteraction(Uri uri);
+    }
+}
