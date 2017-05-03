f9fa97d2b24ff8c857c600463afaf677a98c89df
Dema Pavlovskiy <DemaPavlovskiy@gmail.com>
2017-05-02 23:26:25 -0700

    Added code for getting and making basic text reviews. Untested.

--- app/src/main/java/group1/tcss450/uw/edu/picreview/MainActivity.java
+++ app/src/main/java/group1/tcss450/uw/edu/picreview/MainActivity.java
@@ -1,9 +1,19 @@
 package group1.tcss450.uw.edu.picreview;
 
 import android.net.Uri;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
+import android.widget.Toast;
+
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.net.URLEncoder;
 
 /**
  * Credentials to SQL database. Use SQLyog or MYSQL workbench to login.
@@ -41,6 +51,100 @@ public class MainActivity extends AppCompatActivity implements LoginFragment.OnF
         else if (toStart.equals(getString(R.string.toStartRegisterFragment)))
         {
             getSupportFragmentManager().beginTransaction().replace(R.id.fragmentContainer, new RegisterFragment()).addToBackStack(null).commit();
+        }
+    }
+
+
+
+
+
+
+    // Transfer these over later to proper fragment
+    private class saveNewReviewService extends AsyncTask<String, Void, String> {
+        private final String SERVICE = "http://cssgate.insttech.washington.edu/" +
+                "~demyan15/" + "makeReview.php";
+        @Override
+        protected String doInBackground(String... strings) {
+            if (strings.length != 3) {
+                throw new IllegalArgumentException("Three String arguments required.");
+            }
+            String response = "";
+            HttpURLConnection urlConnection = null;
+            try {
+                URL urlObject = new URL(SERVICE);
+                urlConnection = (HttpURLConnection) urlObject.openConnection();
+                urlConnection.setRequestMethod("POST");
+                urlConnection.setDoOutput(true);
+                OutputStreamWriter wr = new OutputStreamWriter(urlConnection.getOutputStream());
+                String data = URLEncoder.encode("Username", "UTF-8")
+                        + "=" + URLEncoder.encode(strings[0], "UTF-8")
+                        + "&" + URLEncoder.encode("tag", "UTF-8")
+                        + "=" + URLEncoder.encode(strings[1], "UTF-8")
+                        + "&" + URLEncoder.encode("reviewText", "UTF-8")
+                        + "=" + URLEncoder.encode(strings[2], "UTF-8");
+                wr.write(data);
+                wr.flush();
+                InputStream content = urlConnection.getInputStream();
+                BufferedReader buffer = new BufferedReader(new InputStreamReader(content));
+                String s = "";
+                while ((s = buffer.readLine()) != null) {
+                    response += s;
+                }
+            } catch (Exception e) {
+                response = "Unable to connect, Reason: "
+                        + e.getMessage();
+            } finally {
+                if (urlConnection != null)
+                    urlConnection.disconnect();
+            }
+            return response;
+        }
+        @Override
+        protected void onPostExecute(String result) {
+           // Toast.makeText(getContext(), "Result: " + result, Toast.LENGTH_LONG)
+           //         .show();
+        }
+    }
+
+    private class getUserReviewsService extends AsyncTask<String, Void, String> {
+        private final String SERVICE = "http://cssgate.insttech.washington.edu/" +
+                "~demyan15/" + "getUserReviews.php";
+        @Override
+        protected String doInBackground(String... strings) {
+            if (strings.length != 1) {
+                throw new IllegalArgumentException("One String arguments required.");
+            }
+            String response = "";
+            HttpURLConnection urlConnection = null;
+            try {
+                URL urlObject = new URL(SERVICE);
+                urlConnection = (HttpURLConnection) urlObject.openConnection();
+                urlConnection.setRequestMethod("POST");
+                urlConnection.setDoOutput(true);
+                OutputStreamWriter wr = new OutputStreamWriter(urlConnection.getOutputStream());
+                String data = URLEncoder.encode("Username", "UTF-8")
+                        + "=" + URLEncoder.encode(strings[0], "UTF-8");
+                wr.write(data);
+                wr.flush();
+                InputStream content = urlConnection.getInputStream();
+                BufferedReader buffer = new BufferedReader(new InputStreamReader(content));
+                String s = "";
+                while ((s = buffer.readLine()) != null) {
+                    response += s;
+                }
+            } catch (Exception e) {
+                response = "Unable to connect, Reason: "
+                        + e.getMessage();
+            } finally {
+                if (urlConnection != null)
+                    urlConnection.disconnect();
+            }
+            return response;
+        }
+        @Override
+        protected void onPostExecute(String result) {
+           // Toast.makeText(getContext(), "Result: " + result, Toast.LENGTH_LONG)
+           //         .show();
         }
     }
 }
